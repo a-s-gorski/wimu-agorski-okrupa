7c0de892908d653b708fc61b5072080e8f1dcd3f
kolga <kolga3211@gmail.com>
2024-01-15 23:37:15 +0100

    Add Prototypical net ksoft with distrator

--- src/types/config.py
+++ src/types/config.py
@@ -8,10 +8,12 @@ class DatasetConfig(BaseModel):
     n_way: Optional[int] = 3
     n_support: Optional[int] = 5
     n_query: Optional[int] = 20
+    n_unlabeled: Optional[int] = 0
+    n_distractor: Optional[int] = 0
     n_train_episodes: Optional[int] = 1000
     n_val_episodes: Optional[int] = 50
     num_workers: Optional[int] = -1
-    model_type: Optional[Literal["protonet", "softkmeans"]] = "protonet"
+    model_type: Optional[Literal["protonet", "softkmeans", "softkmeansdistractor"]] = "protonet"
 
     def __repr__(self):
         repr_dict = {k: v for k, v in self.model_dump().items()
@@ -26,7 +28,7 @@ class TrainingConfig(BaseModel):
     val_check_interval: Optional[int] = 50
     profiler: Optional[Literal["simple"]] = "simple"
     logger: Optional[Literal["tensorboard"]] = "tensorboard"
-    model_type: Optional[Literal["protonet", "softkmeans"]] = "protonet"
+    model_type: Optional[Literal["protonet", "softkmeans", "softkmeansdistractor"]] = "protonet"
 
     def __repr__(self):
         repr_dict = {k: v for k, v in self.model_dump().items()
--- src/types/model.py
+++ src/types/model.py
@@ -1,20 +1,25 @@
 from enum import Enum
 from music_fsl.protonet import PrototypicalNet
 
-from src.data.episode_dataset import EpisodeDataset, EpisodeDatasetUnlabeled
+from src.data.episode_dataset import EpisodeDataset, EpisodeDatasetUnlabeled, EpisodeDatasetUnlabeledWithDistractor
 from src.models.few_shot_learner import FewShotLearner
 from src.models.few_shot_learner_ksoft import FewShotLearnerKSoft
+from src.models.few_shot_learner_ksoft_distractor import FewShotLearnerUnlabeledWithDistractor
 from src.models.prototypical_net_ksoft import PrototypicalNetKSoft
+from src.models.prototypical_net_ksoft_distractor import PrototypicalNetKSoftWithDistractor
 
 
 class ModelEpisodeType(Enum):
     protonet = EpisodeDataset
     softkmeans = EpisodeDatasetUnlabeled
+    softkmeansdistractor = EpisodeDatasetUnlabeledWithDistractor
 
 class PrototypicalNetType(Enum):
     protonet = PrototypicalNet
     softkmeans = PrototypicalNetKSoft
+    softkmeansdistractor = PrototypicalNetKSoftWithDistractor
 
 class FewShotLearnerType(Enum):
     protonet = FewShotLearner
     softkmeans = FewShotLearnerKSoft
+    softkmeansdistractor = FewShotLearnerUnlabeledWithDistractor
